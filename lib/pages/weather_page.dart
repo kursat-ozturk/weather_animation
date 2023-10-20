084be4826b1ec93513d0aca8f1e2aa09cd3af6d0
Kürşat Öztürk <kursatsoftware@gmail.com>
2023-10-20 11:07:56 +0300

    added lotie animation

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:weather_animation/services/weather_service.dart';
 
 import '../models/weather_model.dart';
@@ -11,7 +13,6 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
-
   // api key
   final _weatherService = WeatherService('YOUR_API_KEY');
   Weather? _weather;
@@ -31,11 +32,36 @@ class _WeatherPageState extends State<WeatherPage> {
 
     // any errors
     catch (e) {
-      print(e);
+      if (kDebugMode) {
+        print(e);
+      }
     }
   }
 
   // weather animations
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json'; // default to sunny
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
 
   // init state
   @override
@@ -49,15 +75,35 @@ class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             // city name
-            Text(_weather?.cityName ?? "loading city..."),
-      
+            Text(
+              _weather?.cityName ?? "loading city...",
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+
+            // animation
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
             // temperature
-            Text('${_weather?.temperature.round()}°C'),
+            Text(
+              '${_weather?.temperature.round()} °C',
+              style: const TextStyle(
+                color: Colors.yellow,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            // weather condition
+            Text(_weather?.mainCondition ?? ""),
           ],
         ),
       ),
